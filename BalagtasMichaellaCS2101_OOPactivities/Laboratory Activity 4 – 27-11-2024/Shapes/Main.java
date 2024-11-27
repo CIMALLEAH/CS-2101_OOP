10134cc9a688fd92935a3df03c86c2f4c9d78f8c
MICHAELLA BALAGTAS <153539219+CIMALLEAH@users.noreply.github.com>
2024-11-27 18:43:55 +0800

    Add files via upload

--- BalagtasMichaellaCS2101_OOPactivities/Laboratory Activity 4 – 27-11-2024/Shapes/Main.java
+++ BalagtasMichaellaCS2101_OOPactivities/Laboratory Activity 4 – 27-11-2024/Shapes/Main.java
@@ -1,1 +1,18 @@
+package Shapes;
 
+public class Main {
+    public static void main(String[] args) {
+
+        Square square = new Square(12);
+        Rectangle rectangle = new Rectangle(20, 15);
+        Triangle triangle = new Triangle(8, 10, 13);
+
+        //collection of shapes
+        Shape[] shapes = {square, rectangle, triangle};
+        ShapeCollection shapeCollection = new ShapeCollection(shapes);
+
+        //for computing and printing the perimeter and area
+        shapeCollection.getPropertyValues();
+
+    }
+}
